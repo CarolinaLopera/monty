f81b386b633bdeca2c6128a91b2d6c10f882c74f
LuisaPinillos <luisapinillos20@gmail.com>
2021-09-21 20:13:03 -0500

    file with the opcode functions

---
+++ func_opcode.c
@@ -0,0 +1,48 @@
+#include "monty.h"
+
+/**
+ * op_pint - Prints the value at the top of the stack,
+ * followed by a new line.
+ */
+
+void op_pint(void)
+{
+	printf("entro a la funcion pint");
+}
+
+/**
+ * op_pall -  Prints all the values on the stack
+ * starting from the top of the stack.
+ */
+
+void op_pall(void)
+{
+	printf("entro a la funcion pall");
+}
+
+/**
+ * op_pop - Removes the top element of the stack.
+ */
+
+void op_pop(void)
+{
+	printf("entro a la funcion pop");
+}
+
+/**
+ * op_swap - Swap The top two elements of the stack.
+ */
+
+void op_swap(void)
+{
+	printf("entro a la funcion swap")
+}
+
+/**
+ * op_add - Add the top two elements of the stack.
+ */
+
+void op_add(void)
+{
+	printf("entro a la funcion add");
+}
