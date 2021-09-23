5eaf69e433ac80b1fab764ca2b4971e5856fc6b2
LuisaPinillos <luisapinillos20@gmail.com>
2021-09-23 09:53:44 -0500

    add others error functions

--- errors.c
+++ errors.c
@@ -2,12 +2,92 @@
 
 /**
  * no_open - write a error if the file can't open
+ *
+ * Return: Always void.
  * @path: the rout of the monty file
  */
 void no_open(char *path)
 {
-	write(STDERR_FILENO, "Error: Can't open file <", 24);
+	write(STDERR_FILENO, "Error: Can't open file ", 23);
 	write(STDERR_FILENO, path, strlen(path));
-	write(STDERR_FILENO, ">\n", 2);
+	write(STDERR_FILENO, "\n", 1);
+	exit(EXIT_FAILURE);
+}
+
+/**
+ * no_file - write a error if argv != 2
+ *
+ * Return: Always void.
+ */
+void no_file(void)
+{
+	write(STDERR_FILENO, "USAGE: monty file\n", 18);
+	exit(EXIT_FAILURE);
+}
+
+/**
+ * no_file - write a error if there is no argument given to push
+ * or if it is is not a integer.
+ *
+ * Return: Always void.
+ * @line_number: Is a number line 
+ */
+void no_int(int line_number)
+{
+	write(STDERR_FILENO, "L", 1);
+	print_int(line_number);
+	write(STDERR_FILENO, ": usage: push integer\n", 22);
+	exit(EXIT_FAILURE);
+}
+
+/**
+ * no_command - write a error if there is no argument given to push
+ * or if it is is not a integer.
+ *
+ * Return: Always void.
+ */
+void no_command(int line_number, char *s)
+{
+	write(STDERR_FILENO, "L", 1);
+	print_int(line_number);
+	write(STDERR_FILENO, ": unknown instruction ", 22);
+	write(STDERR_FILENO, s, strlen(s));
+	write(STDERR_FILENO, "\n", 1);
+	exit(EXIT_FAILURE);
+}
+
+void stack_empty(int line_number)
+{
+	write(STDERR_FILENO, "L", 1);
+	print_int(line_number);
+	write(STDERR_FILENO, ": can't pint, stack empty", 24);
+	write(STDERR_FILENO, "\n", 1);
+	exit(EXIT_FAILURE);
+}
+
+void stack_emptypop(int line_number)
+{
+	write(STDERR_FILENO, "L", 1);
+	print_int(line_number);
+	write(STDERR_FILENO, ": can't pop an empty stack", 26);
+	write(STDERR_FILENO, "\n", 1);
+	exit(EXIT_FAILURE);
+}
+
+void lesstwoelements(int line_number)
+{
+	write(STDERR_FILENO, "L", 1);
+	print_int(line_number);
+	write(STDERR_FILENO, ": can't add, stack too short", 27);
+	write(STDERR_FILENO, "\n", 1);
+	exit(EXIT_FAILURE);
+}
+
+void lesstwoelementsswap(int line_number)
+{
+	write(STDERR_FILENO, "L", 1);
+	print_int(line_number);
+	write(STDERR_FILENO, ": can't swap, stack too short", 28);
+	write(STDERR_FILENO, "\n", 1);
 	exit(EXIT_FAILURE);
 }
