69a107c3dce513902b2008bb69f5025c42774097
LuisaPinillos <luisapinillos20@gmail.com>
2021-09-24 00:34:22 -0500

    Upgrade the structure

--- get_opcode.c
+++ get_opcode.c
@@ -1,9 +1,49 @@
 #include "monty.h"
 
 /**
+ * op_push -  Prints all the values on the stack
+ * starting from the top of the stack.
+ *
+ * Return: A pointer to new node.
+ * @head: Is a header of the list.
+ * @line_number: Is a number line.
+ * @words: Is a array of the words with the commands.
+ */
+stack_t *op_push(stack_t **head, char **words, int line_number)
+{
+	int num, i;
+	stack_t *new_node = malloc(sizeof(stack_t));
+
+	if (new_node == NULL)
+		no_malloc();
+
+	if (words[1] == NULL)
+		no_int(line_number);
+
+	for (i = 0; words[1][i] != '\0'; i++)
+	{
+		if (isdigit(words[1][i]) == 0)
+			no_int(line_number);
+	}
+	num = atoi(words[1]);
+
+	new_node->n = num;
+	new_node->next = (*head);
+	new_node->prev = NULL;
+
+	if (*head != NULL)
+		(*head)->prev = new_node;
+	(*head) = new_node;
+
+	return (new_node);
+}
+
+/**
  * get_opcode - compare the opcode with corresponding function.
- * @s: Opcode.
- * Return: the opcode.
+ * @s: Is the command.
+ * @stack: Is a header of the list.
+ * @line_number: Is a number line.
+ * Return: Always void.
  */
 void get_opcode(stack_t **stack, char *s, unsigned int line_number)
 {
@@ -16,6 +56,10 @@ void get_opcode(stack_t **stack, char *s, unsigned int line_number)
 		{"swap", op_swap},
 		{"add", op_add},
 		{"nop", op_nop},
+		{"sub", op_sub},
+		{"div", op_div},
+		{"mul", op_mul},
+		{"pchar", op_pchar},
 		{NULL, NULL}};
 
 	while (ops[iterator].opcode != NULL)
@@ -31,48 +75,13 @@ void get_opcode(stack_t **stack, char *s, unsigned int line_number)
 }
 
 /**
- * op_push -  Prints all the values on the stack
- * starting from the top of the stack.
+ * op_nop - This function doesn’t do anything.
+ *
+ * Return: Always void.
+ * @stack: Is a pointer to header of the list.
+ * @line_number: Is a current number line.
  */
-stack_t *op_push(stack_t **head, char **words, int line_number)
-{
-	int num;
-	stack_t *new_node = malloc(sizeof(stack_t));
-
-	/*printf("entro a la funcion push\n");*/
-	if (new_node == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-
-	if (words[1] == NULL)
-		no_int(line_number);
-
-	num = atoi(words[1]);
-	if (num == 0)
-		no_int(line_number);
-
-	new_node->n = num;
-	new_node->next = (*head);
-	new_node->prev = NULL;
-
-	if (*head != NULL)
-		(*head)->prev = new_node;
-	(*head) = new_node;
-
-	/*if (new_node->prev != NULL)
-		printf("prev: %i\n", new_node->prev->n);
-	if (new_node->next != NULL)
-		printf("next: %i\n", new_node->next->n);
-	printf("new: %i\n", new_node->n);
-	printf("salió de la funcion push\n");*/
-
-	return (new_node);
-}
-
 void op_nop(stack_t **stack, unsigned int line_number)
 {
-	(void)stack;
-	(void)line_number;
+	(void)stack, (void)line_number;
 }
